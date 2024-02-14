b6c919519467f58c261e9a39fb2d07820ede26b6
sgurrola <124643885+sgurrola@users.noreply.github.com>
2024-02-14 11:13:17 -0600

    add silly

--- main.rs
+++ main.rs
@@ -28,6 +28,10 @@ macro_rules! as_much_as_you_want {
 fn main() {
     thrice!("Hi");
     as_much_as_you_want!(4, "wassup");
+
+    //as_much_as_you_want!("dog", "cat"); 
+    //i include bc I was worried what would happen if put in a not number since macros don't have an easy way to be type specific
+    //but I think the for loop and inner implications imposed that on the times:expr
     
 }
 
